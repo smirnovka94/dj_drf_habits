298811a1ed4e639bf771494e040745261e923f1c
kirill <lekseich8@gmail.com>
2023-11-25 07:14:40 +0300

    добавил эндпоинт создания юзера

--- users/urls.py
+++ users/urls.py
@@ -5,13 +5,14 @@ from rest_framework_simplejwt.views import (
 )
 
 from users.apps import UsersConfig
-from users.views import UserListAPIView, UserUpdateAPIView, UserRetrieveAPIView
+from users.views import UserListAPIView, UserUpdateAPIView, UserRetrieveAPIView, UserCreateAPIView
 
 app_name = UsersConfig.name
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', UserListAPIView.as_view(), name='users_list'),
+    path('create/', UserCreateAPIView.as_view(), name='user_create'),
     path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='users_update'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='users_view'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,9 @@ class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
