d4c336ec6c42a38d7315fd9617e916ff1c2d2e11
kirill <lekseich8@gmail.com>
2023-11-27 12:27:24 +0300

    Не работает генерация Токена

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,45 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+# class UserRegisterSerializer(serializers.ModelSerializer):
+    # password = serializers.CharField(write_only=True, validators=[validate_password])
+    # tokens = serializers.SerializerMethodField()
+    #
+    # def get_tokens(self, user):
+    #     refresh = RefreshToken.for_user(user)
+    #     return {
+    #         'refresh': str(refresh),
+    #         'access': str(refresh.access_token),
+    #     }
+    #
+    # class Meta:
+    #     model = User
+    #     fields = ['username', 'email', 'password', 'tokens']
+
+########
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """Переопределям метод для записи хэшированного пароля в БД"""
+
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data, is_active=True)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- users/views.py
+++ users/views.py
@@ -1,13 +1,19 @@
 from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserRegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 class UserCreateAPIView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserRegisterSerializer
+
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     serializer_class = UserSerializer
@@ -16,3 +22,4 @@ class UserUpdateAPIView(generics.UpdateAPIView):
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
