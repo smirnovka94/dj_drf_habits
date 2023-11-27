9df7da6cd120dcf3730a3ce34fa9cd1f23ce5112
kirill <lekseich8@gmail.com>
2023-11-27 15:36:24 +0300

    Clearing code

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,4 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
-
 from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
